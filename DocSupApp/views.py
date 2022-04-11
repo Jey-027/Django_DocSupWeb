9d5fc0df8d2219b889905a88356b53278bc338f1
jey-027 <jdrodriguezg25@hotmail.com>
2022-04-11 15:15:43 -0500

    archivo modificado gendoc nuevas variables

--- DocSupApp/views.py
+++ DocSupApp/views.py
@@ -94,24 +94,6 @@ class DetFactUpdate(LoginRequiredMixin,UpdateView):
 
     success_url = "/facturas/list"
 
-
-# def updateDocumento(request, id):
-#     doc = documento.objects.get(id = id)
-
-#     if request.method == "GET":
-#         form = documentoForm(instance=doc)
-#     else:
-#         form = documentoForm(request.POST, instance=doc)
-#         if form.is_valid():
-#             doc.status = 1
-#             form.save()
-#             ocreatefile = CreateFile()
-#             ocreatefile.genera_documento()
-#         return redirect("Detalle_facturacion")
-#     return render(request, "DocSupApp/generacion_documento.html", {"form": form})
-
-
-
 def updateDocumento(request, id):
     doc = documento.objects.get(id = id)
 
@@ -123,37 +105,25 @@ def updateDocumento(request, id):
             doc.status = 1
             form.save()
             
-        f = open("C:/load/txt/pruebaDjangofile501.txt" ,"w+")
+        f = open("C:/load/txt/pruebaDjangofile502.txt" ,"w+")
         f.write("ENC,DS,DIAN 2.1: Documento soporte en adquisiciones efectuadas a no obligados a facturar.,DME1503," + time.strftime('%Y-%m-%d,%H:%M:%S', time.localtime()) +"-05:00," + "05,COP,1," + "%s"%doc.payment_date + ",2,10,UBL 2.1\n")
-        f.write("CUD,123456789ASD0987654321\n")
-        f.write("EMI,1,,11001,Bogotá D.C.,110111,Bogotá,11,CRA 72 80-94 OF 902 CTRO EMP. TITAN PLAZA,CO,Colombia,,Black & Decker de Colombia S.A.S,935462718,1,31 \n")
-        f.write("TAC,O-13\n")
-        f.write("GTE,01,IVA\n")   
-        f.write("ADQ,1,,,,,,,,,," + "%s"%doc.Nit + ",, " + "%s"%doc.type_of_tax_number + "," + "%s"%doc.suplier_tax_code + "\n")
-        f.write("TCR,O-13;O-15\n")  ## VALIDAR SI ES 13 0 15
+        f.write("CUD,\n")
+        f.write("EMI,2,," + "%s"%doc.city_id + "," +"%s"%doc.city_name + "," + "%s"%doc.city_id + "%s"%doc.est_fed_prov + "," + "%s"%doc.city_name + "," + "%s"%doc.est_fed_prov + "," + "%s"%doc.address + "," + "%s"%doc.country + ",Colombia,," + "%s"%doc.name_supplier_vendor + "," + "%s"%doc.Nit + ",DV," + "%s"%doc.type_of_tax_number + "\n") # informacion del proveedor
+        f.write("TAC,R-99-PN\n")
+        f.write("GTE,ZZ,IVA\n")   
+        f.write("ADQ,1,,,,,,,,,,860070698,,1,31,Black & Decker de Colombia S.A.S\n") 
+        f.write("TCR,O-13\n")
         f.write("GTA,01,IVA\n") 
         f.write("TOT," + "%s"%doc.net_amount + ",COP," + "%s"%doc.net_amount  + ",COP," + "%s"%doc.net_amount  + ",COP," + "%s"%doc.net_amount  + ",COP,0.00,COP,0.00,COP,,,,\n")
-        f.write("TIM,false,0.00,COP\n")
+        f.write("TIM,true,0.00,COP\n")
         f.write("IMP,01," + "%s"%doc.net_amount  + ",COP," + "%s"%doc.tax_amount + ",COP,19.00\n")	
-        f.write("TIM,true,4000.00,COP\n") ## VALIDAR
-        f.write("IMP,06,100000.00,COP,4000.00,COP,4.00\n") ## VALIDAR
-        f.write("DSC,false,0,0.00,COP,01,," + "%s"%doc.net_amount + ",COP,1 \n")
-        f.write("DRF,32912731242012,2019-11-26,2022-11-26,DME,1,5000000\n") ## VALIDAR
+        f.write("DRF,19890900900,2019-01-19,2030-01-19,DSA,5000001,8000000\n")
         f.write("NOT,1_Responsable de impuesto sobre las ventas - IVA - Agentes Retenedores de IVA.\n")
-        f.write("MEP,1,2,2020-06-26\n") ## CREAR UNA TABLA DE LOS CODIGOS DE METODO DE PAGO Y TRAER LOS DATOS DE AHI Y VALIDAR SI LA FECHA DE CREDITO DEBE INSERTAR EL USUARIO
-        f.write("IDP,IDP1\n")
-        f.write("CTS,CE01\n")
-        f.write("ITE,1,1,94," + "%s"%doc.net_amount  + ",COP," + "%s"%doc.net_amount  + ",COP,," + "%s"%doc.item_description + ",1,VALIDARCODIGOVENDEDOR,1,94,," + "%s"%doc.net_amount + ",COP,,,,\n")
+        f.write("MEP,1,2," +  "deberia ir el payment date" + "2020-06-26\n") ## CREAR UNA TABLA DE LOS CODIGOS DE METODO DE PAGO Y TRAER LOS DATOS DE AHI Y VALIDAR SI LA FECHA DE CREDITO DEBE INSERTAR EL USUARIO
+        f.write("ITE,1,1,94," + "%s"%doc.net_amount  + ",COP," + "%s"%doc.net_amount  + ",COP,," + "%s"%doc.item_description + ",1,VALIDARCODIGOVENDEDOR,1,94,," + "%s"%doc.net_amount + ",COP,,,,\n") # valdia codigo
         f.write("FCB," + "%s"%doc.date_Invoice + ",1,Por operación\n")
-        f.write("MYM,Carvajal,CTS\n") # PREGUNTAR CARVAJAL
-        f.write("IAE,12345,999,,Estándar de adopción del contribuyente\n") # PREGUNTAR CARVAJAL
         f.write("TII," + "%s"%doc.tax_amount + ",COP,false\n")
-        f.write("IIM,01," + "%s"%doc.tax_amount + ",COP," + "%s"%doc.net_amount + ",COP,19.00\n") # validar IVA
-        f.write("DIT," + "%s"%doc.tax_amount + "\n")
-        f.write("NTI,\n")
-        f.write("NTI,\n")
-        f.write("NTI,\n")
-        f.write("ORP," + "%s"%doc.id + "," + "%s"%doc.payment_date + "\n")
+        f.write("IIM,01," + "%s"%doc.tax_amount + ",COP," + "%s"%doc.net_amount + ",COP,19.00\n")
         f.close()
 
         return redirect("Detalle_facturacion")
